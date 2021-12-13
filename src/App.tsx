3d903a2f2b629e41df3e20fdc40a28dd6fe5e02d
jackykwandesign <jackykwandesign@gmail.com>
2021-12-13 10:19:16 +0000

    remove useless code in cra

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,6 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import ConfigedFirebase from './config/firebaseConfig'
 import FirebaseFileUploader from './components/FirebaseFileUploader';
-// import firebase from 'firebase/compat';
 import { getStorage, ref} from 'firebase/storage';
 import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 
@@ -19,25 +16,6 @@ function App() {
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <path style={{fill:"red"}}>
-      <img src={"http://s.cdpn.io/3/kiwi.svg"} style={{fill:"red"}}/>
-
-
-      </path>
       
       <button onClick={async ()=>{
         await signInWithPopup(auth, provider)
